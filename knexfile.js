fdbd2c35d008d4f1ed2baebd006d89ce30c830bc
Ricardo Pereira <ricardo.pereira@sisloc.com.br>
2023-04-25 23:49:22 -0300

    rest-full-db: agendas

--- knexfile.js
+++ knexfile.js
@@ -5,37 +5,13 @@
  */
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: {
-      host: 'db-postgresql-nyc1-krinfo-01-do-user-10191204-0.b.db.ondigitalocean.com',
-      database: 'rest-full-pool',
-      user:     'doadmin',
-      password: 'AVNS_govTz9ZpnVjrDLMkvzX',
+      host: '',
+      database: '',
+      user:     '',
+      password: '',
       port: 25061,
       ssl: {
         rejectUnauthorized: false
